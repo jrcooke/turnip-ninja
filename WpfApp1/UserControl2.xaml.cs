a57b8be47f460b21d04efdb23c968126308d003f
Jason Cooke <jrcooke2001@hotmail.com>
2019-01-02 10:15:13 -0800

    Chop up mesh before simplifying, to stop thrashing RAM. TODO: Figure out how to glue chunks of mesh back together Fix bug where corners get pulled in, resulting in empty star shapes where 4 chunks meet

--- WpfApp1/UserControl2.xaml.cs
+++ WpfApp1/UserControl2.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using MeshDecimator;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -60,75 +62,79 @@ namespace WpfApp1
             Material myMaterial = new DiffuseMaterial(ib);
             myGeometryModel.Material = myMaterial;
 
-            Point3DCollection myPositionCollection = new Point3DCollection();
-            var max = heights.Length;
-            for (int i = 0; i < max; i++)
-            {
-                for (int j = 0; j < max; j++)
-                {
-                    int iPrime = (max - 1 - i) * heights.Length / max;
-                    int jPrime = (j) * heights[0].Length / max;
-                    double height = 10000 * heights[jPrime][iPrime];
-                    myPositionCollection.Add(new Point3D(
-                        10.0 * (i - max / 2.0) * imageWidth / (max * imageHeight),
-                        10.0 * (j - max / 2.0) / max,
-                        10.0 * height / (max * imageHeight)));
-                }
-            }
-
-            // Create a collection of triangle indices for the MeshGeometry3D.
-            Int32Collection myTriangleIndicesCollection = new Int32Collection();
-            for (int i = 0; i < (max - 1); i++)
-            {
-                for (int j = 0; j < (max - 1); j++)
-                {
-                    myTriangleIndicesCollection.Add((i + 0) * max + (j + 0));
-                    myTriangleIndicesCollection.Add((i + 1) * max + (j + 0));
-                    myTriangleIndicesCollection.Add((i + 0) * max + (j + 1));
-                    myTriangleIndicesCollection.Add((i + 1) * max + (j + 1));
-                    myTriangleIndicesCollection.Add((i + 0) * max + (j + 1));
-                    myTriangleIndicesCollection.Add((i + 1) * max + (j + 0));
-                }
-            }
-
-            SimpleMesh sm = new SimpleMesh(myPositionCollection, myTriangleIndicesCollection);
-            myPositionCollection = null;
-            myTriangleIndicesCollection = null;
-            sm.Simplify();
-
             MeshGeometry3D myMeshGeometry3D = new MeshGeometry3D();
-            myMeshGeometry3D.Positions = sm.Positions;
-            myMeshGeometry3D.TextureCoordinates = new PointCollection(sm.Positions.Select(p => new Point(p.X, -p.Y)));
-            myMeshGeometry3D.TriangleIndices = sm.TriangleIncides;
+            myMeshGeometry3D.Positions = new Point3DCollection();
+            myMeshGeometry3D.TextureCoordinates = new PointCollection();
+            myMeshGeometry3D.TriangleIndices = new Int32Collection();
+
+            int numChunks = 5;
+            var max = heights.Length;
+            var chunkMax = max / numChunks;
+            for (int chunkI = 0; chunkI < numChunks; chunkI++)
+            {
+                int iMin = chunkI * chunkMax;
+                int iMax = (chunkI < numChunks - 1 ? chunkMax * (chunkI + 1) + 1 : max);
+                for (int chunkJ = 0; chunkJ < numChunks; chunkJ++)
+                {
+                    int jMin = chunkJ * chunkMax;
+                    int jMax = (chunkJ < numChunks - 1 ? chunkMax * (chunkJ + 1) + 1 : max);
+                    List<Vector3d> positions = new List<Vector3d>();
+                    for (int i = iMin; i < iMax; i++)
+                    {
+                        for (int j = jMin; j < jMax; j++)
+                        {
+                            int iPrime = (max - 1 - i) * heights.Length / max;
+                            int jPrime = (j) * heights[0].Length / max;
+                            double height = 10000 * heights[jPrime][iPrime];
+                            positions.Add(new Vector3d(
+                                10.0 * (i - max / 2.0) * imageWidth / (max * imageHeight),
+                                10.0 * (j - max / 2.0) / max,
+                                10.0 * height / (max * imageHeight)));
+                        }
+                    }
+
+                    // Create a collection of triangle indices for the MeshGeometry3D.
+                    List<int> TriangleIncides = new List<int>();
+                    for (int i = 0; i < iMax - iMin - 1; i++)
+                    {
+                        for (int j = 0; j < jMax - jMin - 1; j++)
+                        {
+                            TriangleIncides.Add((i + 0) * (jMax - jMin) + (j + 0));
+                            TriangleIncides.Add((i + 1) * (jMax - jMin) + (j + 0));
+                            TriangleIncides.Add((i + 0) * (jMax - jMin) + (j + 1));
+                            TriangleIncides.Add((i + 1) * (jMax - jMin) + (j + 1));
+                            TriangleIncides.Add((i + 0) * (jMax - jMin) + (j + 1));
+                            TriangleIncides.Add((i + 1) * (jMax - jMin) + (j + 0));
+                        }
+                    }
+
+                    var md = new SimplifyMesh(positions.ToArray(), TriangleIncides.ToArray());
+                    positions = null;
+                    TriangleIncides = null;
+
+                    md.SimplifyMeshByThreshold(1.0E-3);
+                    var ps = md.GetVertices();
+                    var tis = md.GetIndices();
+                    md = null;
+
+                    int positionOffset = myMeshGeometry3D.Positions.Count;
+
+                    foreach (var p in ps)
+                    {
+                        myMeshGeometry3D.Positions.Add(new Point3D(p.X, p.Y, p.Z));
+                        myMeshGeometry3D.TextureCoordinates.Add(new Point(p.X, -p.Y));
+                    }
+                    ps = null;
+
+                    foreach (var ti in tis)
+                    {
+                        myMeshGeometry3D.TriangleIndices.Add(ti + positionOffset);
+                    }
+                    tis = null;
+                }
+            }
+
             myGeometryModel.Geometry = myMeshGeometry3D;
-        }
-
-        private class SimpleMesh
-        {
-            public Point3DCollection Positions { get; set; }
-            public Int32Collection TriangleIncides { get; set; }
-
-            public SimpleMesh(Point3DCollection positions, Int32Collection triangleIncides)
-            {
-                Positions = positions;
-                TriangleIncides = triangleIncides;
-            }
-
-            public void Simplify()
-            {
-
-                var sourceVertices = Positions.Select(p => new Vector3d(p.X, p.Y, p.Z)).ToArray();
-                var md = new SimplifyMesh(sourceVertices, TriangleIncides.ToArray());
-                Positions = null;
-                TriangleIncides = null;
-
-                md.SimplifyMeshByThreshold(1.0E-3);
-                var newVertices = md.GetVertices();
-                var newIndices = md.GetIndices();
-
-                Positions = new Point3DCollection(newVertices.Select(p => new Point3D(p.X, p.Y, p.Z)));
-                TriangleIncides = new Int32Collection(newIndices);
-            }
         }
     }
 }
