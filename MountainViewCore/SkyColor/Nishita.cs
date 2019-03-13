df7740e74d65edf5b5f8882ce16b16ad64aed311
Jason Cooke <jrcooke2001@hotmail.com>
2019-03-13 07:49:21 -0700

    Sky color

---
+++ MountainViewCore/SkyColor/Nishita.cs
@@ -0,0 +1,429 @@
+﻿using MountainView.Base;
+using MountainView.Mesh;
+using System;
+
+namespace MountainView.SkyColor
+{
+    public class Nishita
+    {
+        private readonly double thetaSun;
+        private readonly double phiSun;
+        private readonly double sinThetaSun;
+        private readonly double cosThetaSun;
+        private readonly double sinPhiSun;
+        private readonly double cosPhiSun;
+        private readonly double turbidity;
+
+        public const double H_atmosphere = 60000;
+
+        public enum Channel
+        {
+            R = 0, // lambda = 680nm
+            G = 1, // lambda = 550nm
+            B = 2, // lambda = 440nm
+        }
+
+        // Units are meters
+        public const double H_R = 8000.0;
+
+        // Units are 1/meters
+        private static readonly double[] BetaR0 = new double[]
+        {
+             05.8e-6, // R
+             13.5e-6, // G
+             33.1e-6, // B
+        };
+
+        // meters
+        public const double H_M = 1200.0;
+
+        // Units are 1/meters
+        private const double BetaM0 = 210e-5;
+
+
+        /// <param name="turbidity">2.2 is clear sky</param>
+        public Nishita(GeoPolar2d sunPos, double turbidity) // = 2.2)
+        {
+            //thetaSun = sunPos.Lon.Radians;
+            //phiSun = sunPos.Lat.Radians;
+            thetaSun = sunPos.Lat.Radians;
+            phiSun = sunPos.Lon.Radians;
+            this.turbidity = turbidity;
+
+
+            sinThetaSun = Math.Sin(thetaSun);
+            sinPhiSun = Math.Sin(phiSun);
+            cosThetaSun = Math.Cos(thetaSun);
+            cosPhiSun = Math.Cos(phiSun);
+        }
+
+        public void RenderToJpeg(string filename)
+        {
+            int width = 1000;
+            int height = 250;
+            MyColor[][] image = new MyColor[width][];
+            for (int thetaI = 0; thetaI < width; thetaI++)
+            {
+                double thetaDeg = thetaI * 360.0 / width;
+                image[thetaI] = new MyColor[height];
+                for (int phiI = 0; phiI < height; phiI++)
+                {
+                    double phiDeg = phiI * 90.0 / height;
+                    var p = new GeoPolar2d(thetaDeg, phiDeg);
+                    image[thetaI][phiI] = SkyColorAtPoint(p);
+                }
+            }
+
+            Utils.WriteImageFile(image, filename, (a) => a, OutputType.JPEG);
+        }
+
+        public MyColor SkyColorAtPoint(GeoPolar2d p)
+        {
+            double h0 = 0.1;
+            //double thetaPixel = p.Lon.Radians;
+            //double phiPixel = p.Lat.Radians;
+            double thetaPixel = p.Lat.Radians;
+            double phiPixel = p.Lon.Radians;
+
+            if (Math.Abs(thetaPixel - thetaSun) < Math.PI / 180 && Math.Abs(phiPixel - phiSun) < Math.PI / 180)
+            {
+                return MyColor.White;
+            }
+
+            double theta = Utils.AngleBetween(thetaPixel, phiPixel, thetaSun, phiSun);
+
+            double sinPhi = Math.Sin(phiPixel);
+            if (sinPhi < 0.0) { return new MyColor(); }
+
+            double cosPhi = Math.Cos(phiPixel);
+            double sinTheta = Math.Sin(thetaPixel);
+            double cosTheta = Math.Cos(thetaPixel);
+            var intersect = Intersect(h0, sinPhi, H_atmosphere);
+            if (!intersect.HasValue) return new MyColor();
+
+            var tot = LTot(h0, intersect.Value, sinPhi, cosPhi, sinTheta, cosTheta, theta);
+
+            double scale = 15;
+            double r = scale * tot.R;
+            double g = scale * tot.G;
+            double b = scale * tot.B;
+
+            if (true)
+            {
+                scale = -45.0;
+                r = 1 - Math.Exp(scale * tot.R);
+                g = 1 - Math.Exp(scale * tot.G);
+                b = 1 - Math.Exp(scale * tot.B);
+            }
+
+            var color = new MyColor(
+                (byte)(255 * Math.Max(0.0, Math.Min(1.0, r))),
+                (byte)(255 * Math.Max(0.0, Math.Min(1.0, g))),
+                (byte)(255 * Math.Max(0.0, Math.Min(1.0, b))));
+
+            return color;
+        }
+
+        public MyDColor LTot(double h0, double l_max, double sinPhiP, double cosPhiP, double sinThetaP, double cosThetaP, double theta)
+        {
+            var rayRR = BetaR0[(int)Channel.R] * P_R(theta);
+            var rayRG = BetaR0[(int)Channel.G] * P_R(theta);
+            var rayRB = BetaR0[(int)Channel.B] * P_R(theta);
+            var rayM = turbidity * BetaM0 * P_M(theta);
+            var integral =
+                Integrate(0, l_max - 10, l => ElementLight(rayRR, rayRG, rayRB, rayM, h0, l, sinPhiP, cosPhiP, sinThetaP, cosThetaP));
+            return integral;
+        }
+
+        // The core ofthe element light
+        // P is the coords of the pixel we are looking at
+        // l is the distance we've traveled
+        // h0 is the height at the starting point
+        private MyDColor ElementLight(
+            double rayRR, double rayRG, double rayRB, double rayM,
+            double h0, double l,
+            double sinPhiP, double cosPhiP,
+            double sinThetaP, double cosThetaP)
+        {
+            // The height of the current element
+            double h = H(h0, sinPhiP, l);
+
+            // The psi is the angular distance across earth to current point
+            double sinPsi = l / (h + h0 + Utils.AlphaMeters) * cosPhiP;
+            double cosPsi = Math.Sqrt(1 - sinPsi * sinPsi);
+
+            // Cosine of angle to sun from up at current element,
+            // which is sin of angle of sun from horizon
+            double sinPhiSunPrime =
+                    sinPsi * sinThetaP * cosPhiSun * sinThetaSun +
+                    sinPsi * cosThetaP * cosPhiSun * cosThetaSun +
+                    cosPsi * sinPhiSun;
+            var hitEarth = Intersect(h, sinPhiSunPrime, 0);
+            if (hitEarth.HasValue && hitEarth.Value > 0)
+            {
+                return new MyDColor();
+            }
+
+            double sunAttenuationRR = 1.0;
+            double sunAttenuationRG = 1.0;
+            double sunAttenuationRB = 1.0;
+            double sunAttenuationM = 1.0;
+            double? lmaxP = Intersect(h, sinPhiSunPrime, H_atmosphere);
+            if (lmaxP.HasValue)
+            {
+                if (lmaxP.Value < 0)
+                {
+                    throw new InvalidOperationException();
+                }
+
+                sunAttenuationRR = Math.Exp(-TR(h, sinPhiSunPrime, lmaxP.Value, Channel.R));
+                sunAttenuationRG = Math.Exp(-TR(h, sinPhiSunPrime, lmaxP.Value, Channel.G));
+                sunAttenuationRB = Math.Exp(-TR(h, sinPhiSunPrime, lmaxP.Value, Channel.B));
+                sunAttenuationM = Math.Exp(-TM(h, sinPhiSunPrime, lmaxP.Value));
+            }
+
+            double densityPartOfScatteringR = Math.Exp(-h / H_R);
+            double densityPartOfScatteringM = Math.Exp(-h / H_M);
+
+            double scatteredAttenuationRR = Math.Exp(-TR(h0, sinPhiP, l, Channel.R));
+            double scatteredAttenuationRG = Math.Exp(-TR(h0, sinPhiP, l, Channel.G));
+            double scatteredAttenuationRB = Math.Exp(-TR(h0, sinPhiP, l, Channel.B));
+            double scatteredAttenuationM = Math.Exp(-TM(h0, sinPhiP, l));
+
+            double totalR = sunAttenuationRR * sunAttenuationM * (rayRR * densityPartOfScatteringR + rayM * densityPartOfScatteringM) * scatteredAttenuationRR * scatteredAttenuationM;
+            double totalG = sunAttenuationRG * sunAttenuationM * (rayRG * densityPartOfScatteringR + rayM * densityPartOfScatteringM) * scatteredAttenuationRG * scatteredAttenuationM;
+            double totalB = sunAttenuationRB * sunAttenuationM * (rayRB * densityPartOfScatteringR + rayM * densityPartOfScatteringM) * scatteredAttenuationRB * scatteredAttenuationM;
+
+            return new MyDColor()
+            {
+                R = totalR,
+                G = totalG,
+                B = totalB,
+            };
+        }
+
+        public static double H(double h0, double sinPhi, double l)
+        {
+            var rprime = Utils.AlphaMeters + h0;
+            // var ln = l / rprime;
+            // return -Utils.AlphaMeters + rprime * Math.Sqrt(1 + 2 * ln * sinPhi + ln * ln);
+            return h0 + l * sinPhi + l * l / (2 * rprime);
+        }
+
+        public static double? Intersect(double h0, double sinPhi, double intersectH)
+        {
+            var rprime = Utils.AlphaMeters + h0;
+            double discriminant = sinPhi * sinPhi + 2 * (intersectH - h0) / rprime;
+            if (discriminant < 0.0) return null;
+
+            double sqrt = Math.Sqrt(discriminant);
+            return rprime * (sqrt - sinPhi);
+        }
+
+        private static double P_R(double theta)
+        {
+            double cosTheta = Math.Cos(theta);
+            return 3.0 / (16.0 * Math.PI) * (1 + cosTheta * cosTheta);
+        }
+
+        private static double P_M(double theta)
+        {
+            double cosTheta = Math.Cos(theta);
+            double g = 0.76;
+            double g2 = g * g;
+            return 3.0 * (1 - g2) * (1 + cosTheta * cosTheta) / (8 * Math.PI * (2 + g2) * Math.Pow(1 + g2 - 2 * g * cosTheta, 1.5));
+        }
+
+        public static double TR(double h0, double sinPhi, double lmax, Channel lambda)
+        {
+            return BetaR0[(int)lambda] * TOverBeta(h0, sinPhi, lmax, H_R);
+        }
+
+        public double TM(double h0, double sinPhi, double lmax)
+        {
+            return turbidity * BetaM0 * TOverBeta(h0, sinPhi, lmax, H_M);
+        }
+
+        public static double TOverBeta(double h0, double sinPhi, double lmax, double H)
+        {
+            if (lmax == 0.0)
+            {
+                return 0;
+            }
+
+            var rprime = Utils.AlphaMeters + h0;
+            double sqrtRPo2H = Math.Sqrt(rprime / (2 * H));
+
+            double i = Math.Exp(-h0 / H) * sqrtRPo2H * H *
+                ErfSPExpXSq(sinPhi * sqrtRPo2H, sinPhi * sqrtRPo2H + sqrtRPo2H * lmax / rprime);
+
+            if (double.IsInfinity(i) || double.IsNaN(i) || double.IsNegativeInfinity(i) || double.IsPositiveInfinity(i))
+            {
+                throw new InvalidOperationException();
+            }
+
+            return i;
+        }
+
+        public static double TOverBetaSimple(double h0, double sinPhi, double charHeight)
+        {
+            double? lmaxN = Intersect(h0, sinPhi, H_atmosphere);
+            if ((lmaxN ?? 0.0) == 0.0)
+            {
+                return 0;
+            }
+
+            var lmax = lmaxN.Value;
+            if (lmax < 0.0)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var i = Integrate(0, lmax, l => Math.Exp(-H(h0, sinPhi, l) / charHeight), 1000);
+            return i;
+        }
+
+        public static double Integrate(double x0, double x1, Func<double, double> f, int num = 15)
+        {
+            double dx = (x1 - x0) / (num - 1);
+
+            double acc = (f(x0) + f(x1)) / 2;
+            for (int i = 1; i < (num - 1); i++)
+            {
+                acc += f(x0 + i * dx);
+            }
+
+            return acc * dx;
+        }
+
+        public static MyDColor Integrate(double x0, double x1, Func<double, MyDColor> f, int num = 15)
+        {
+            double dx = (x1 - x0) / (num - 1);
+
+            MyDColor acc = new MyDColor();
+            MyDColor f0 = f(x0);
+            MyDColor f1 = f(x1);
+            acc.R = (f0.R + f1.R) / 2;
+            acc.G = (f0.G + f1.G) / 2;
+            acc.B = (f0.B + f1.B) / 2;
+
+            for (int i = 1; i < (num - 1); i++)
+            {
+                var fi = f(x0 + i * dx);
+                acc.R += fi.R;
+                acc.G += fi.G;
+                acc.B += fi.B;
+            }
+
+            acc.R *= dx;
+            acc.G *= dx;
+            acc.B *= dx;
+
+            return acc;
+        }
+
+        /// <summary>
+        /// Gives the error function
+        ///     Erf(x) = 2/Sqrt(PI) * Integral(0, x, exp(-t^2) dt)
+        /// </summary>
+        /// <param name="x0">Lower bound on the ERF integral</param>
+        public static double ErfSPExpXSq(double x)
+        {
+            //return Erf(0, x);
+            var erf = ErfSPExpXSqWorker(x);
+            return erf.Item1 + erf.Item2;
+        }
+
+        public static double Erf(double x0, double x1)
+        {
+            var ret = ErfSPExpXSq(x0, x1) * Math.Exp(-x0 * x0) / Math.Sqrt(Math.PI);
+            return ret;
+        }
+
+        private static readonly double SqrtPi = Math.Sqrt(Math.PI);
+
+        /// <summary>
+        /// Gives the error function
+        ///    Sqrt(PI) Exp(x0^2) Erf(x0,x1) = 2 * Exp(x0^2) * Integral(x0, x1, exp(-t^2) dt)
+        /// </summary>
+        /// <param name="x0">Lower bound on the ERF integral</param>
+        /// <param name="x1">Upper bounc on the ERF integral</param>
+        public static double ErfSPExpXSq(double x0, double x1)
+        {
+            var erf0 = ErfSPExpXSqWorker(x0);
+            var erf1 = ErfSPExpXSqWorker(x1);
+            var exp = Math.Exp(x0 * x0 - x1 * x1);
+
+            var ret = exp * erf1.Item1 - erf0.Item1;
+
+            if (erf0.Item2 != erf1.Item2)
+            {
+                ret += (erf1.Item2 - erf0.Item2) * SqrtPi * Math.Exp(x0 * x0);
+            }
+
+            return ret;
+        }
+
+        public static double Erf2(double x)
+        {
+            return Erf2(0, x);
+        }
+
+        /// <summary>
+        /// Gives the error function
+        ///     Erf(x0,x1) = 2/Sqrt(PI) * Integral(x0, x1, exp(-t^2) dt)
+        /// </summary>
+        /// <param name="x0">Lower bound on the ERF integral</param>
+        /// <param name="x1">Upper bounc on the ERF integral</param>
+        public static double Erf2(double x0, double x1)
+        {
+            return 2 / Math.Sqrt(Math.PI) * Integrate(x0, x1, t => Math.Exp(-t * t), 1000);
+        }
+
+        private static Tuple<double, int> ErfSPExpXSqWorker(double x)
+        {
+            if (x == 0.0) return new Tuple<double, int>(0.0, 0);
+
+            if (x < 0)
+            {
+                var ret = ErfSPExpXSqWorker(-x);
+                return new Tuple<double, int>(-ret.Item1, -ret.Item2);
+            }
+
+            // Using equations from http://www.mhtlab.uwaterloo.ca/courses/me755/web_chap2.pdf
+            if (x <= 2.0)
+            {
+                return new Tuple<double, int>(ErfSPExpXSqWorkerSmallX(x), 0);
+            }
+            else
+            {
+                return new Tuple<double, int>(-ErfcSPWorkerLargeX(x), 1);
+            }
+        }
+
+        public static double ErfSPExpXSqWorkerSmallX(double x)
+        {
+            // Using equations from http://www.mhtlab.uwaterloo.ca/courses/me755/web_chap2.pdf
+            double a0 = 1.0;
+            double sum = a0;
+            double anm1 = a0;
+            for (int n = 1; n < 16; n++)
+            {
+                double an = x * x * anm1 * 2 / (2 * n + 1.0);
+                sum += an;
+                anm1 = an;
+            }
+            return 2 * x * sum;
+        }
+
+        public static double ErfcSPWorkerLargeX(double x)
+        {
+            double contFracTerm = 0;
+            for (int n = 16; n >= 1; n--)
+            {
+                contFracTerm = n / (2 * (x + contFracTerm));
+            }
+            return 1 / (x + contFracTerm);
+        }
+    }
+}
---
+++ MountainViewCore/SkyColor/Preetham.cs
@@ -0,0 +1,162 @@
+﻿using MountainView.Base;
+using MountainView.Mesh;
+using System;
+
+namespace MountainViewCore.SkyColor
+{
+    // https://www.cs.utah.edu/~shirley/papers/sunsky/sunsky.pdf
+    // Preetham, A. J. et al. "A Practical Analytic Model for Daylight" SIGGRAPH (1999).
+    public class Preetham
+    {
+        private readonly double thetaSun;
+        private readonly double phiSun;
+        private AllSkylightDistCoef skyParams;
+
+        /// <param name="turbidity">2.2 is clear sky</param>
+        public Preetham(GeoPolar2d sunPos, double turbidity = 2.2)
+        {
+            // Theta is angle from zenith down to sun.
+            // phi is angle around up-axis from south.
+            thetaSun = Math.PI / 2 - sunPos.Lon.Radians;
+            phiSun = Math.PI - sunPos.Lat.Radians;
+            while (phiSun > +Math.PI) phiSun -= 2 * Math.PI;
+            while (phiSun < -Math.PI) phiSun += 2 * Math.PI;
+
+            skyParams = new AllSkylightDistCoef(turbidity, thetaSun);
+        }
+
+        public void RenderToJpeg(string filename)
+        {
+            int width = 1000;
+            int height = 250;
+            MyColor[][] image = new MyColor[width][];
+            for (int phiI = 0; phiI < width; phiI++)
+            {
+                double phiDeg = phiI * 360.0 / width;
+                image[phiI] = new MyColor[height];
+                for (int thetaI = 0; thetaI < height; thetaI++)
+                {
+                    double thetaDeg = thetaI * 90.0 / height;
+                    var p = new GeoPolar2d(phiDeg, thetaDeg);
+                    image[phiI][thetaI] = SkyColorAtPoint(p);
+                }
+            }
+
+            Utils.WriteImageFile(image, filename, (a) => a, OutputType.JPEG);
+        }
+
+        public MyColor SkyColorAtPoint(GeoPolar2d p)
+        {
+            if (p.Lon.Radians < 0.0) return new MyColor();
+            // Theta is angle from zenith down to sun.
+            // phi is angle around up-axis from south.
+            var theta = Math.PI / 2 - p.Lon.Radians;
+            var phi = Math.PI - p.Lat.Radians;
+            while (phi > +Math.PI) phi -= 2 * Math.PI;
+            while (phi < -Math.PI) phi += 2 * Math.PI;
+
+            var Y = SkyChanelAtPoint(skyParams.Y, theta, phi);
+            var x = SkyChanelAtPoint(skyParams._x, theta, phi);
+            var y = SkyChanelAtPoint(skyParams._y, theta, phi);
+
+            // Convert from luminance and chromaticity to RGB.
+            var X = Y / y * x;
+            var Z = Y / y * (1 - x - y);
+
+            var r = +3.241030 * X - 1.537410 * Y - 0.498620 * Z;
+            var g = -0.969242 * X + 1.875960 * Y + 0.041555 * Z;
+            var b = +0.055632 * X - 0.203979 * Y + 1.056980 * Z;
+
+            double scale = -3.0;
+            r = 1 - Math.Exp(scale * r);
+            g = 1 - Math.Exp(scale * g);
+            b = 1 - Math.Exp(scale * b);
+
+            var color = new MyColor(
+                (byte)(255 * Math.Max(0.0, Math.Min(1.0, r))),
+                (byte)(255 * Math.Max(0.0, Math.Min(1.0, g))),
+                (byte)(255 * Math.Max(0.0, Math.Min(1.0, b))));
+            return color;
+        }
+
+        private double SkyChanelAtPoint(SkylightDistCoef skyCoef, double theta, double phi)
+        {
+            var gamma = Math.Acos(
+                Math.Cos(thetaSun) * Math.Cos(theta) +
+                Math.Sin(thetaSun) * Math.Sin(theta) * Math.Cos(phiSun - phi));
+            return skyCoef.Zenith * CurlyF(skyCoef, theta, gamma) / CurlyF(skyCoef, 0, thetaSun);
+        }
+
+        private static double CurlyF(SkylightDistCoef skyCoef, double theta, double gamma)
+        {
+            return
+                (1 + skyCoef.A * Math.Exp(skyCoef.B / Math.Cos(theta))) *
+                (1 + skyCoef.C * Math.Exp(skyCoef.D * gamma) + skyCoef.E * Math.Cos(gamma) * Math.Cos(gamma));
+        }
+
+        private class AllSkylightDistCoef
+        {
+            public SkylightDistCoef _x { get; private set; }
+            public SkylightDistCoef _y { get; private set; }
+            public SkylightDistCoef Y { get; private set; }
+
+            public AllSkylightDistCoef(double turbidity, double thetaSun)
+            {
+                var t1 = thetaSun;
+                var t2 = t1 * t1;
+                var t3 = t1 * t2;
+                double Yz = (4.0453 * turbidity - 4.9710) * Math.Tan((4.0 / 9 - turbidity / 120) * (Math.PI - 2 * thetaSun)) - 0.2155 * turbidity + 2.4192;
+                double Y0 = (4.0453 * turbidity - 4.9710) * Math.Tan((4.0 / 9 - turbidity / 120) * (Math.PI)) - 0.2155 * turbidity + 2.4192;
+                double YZen = Yz / Y0;
+                double xZen =
+                    (+0.00166 * t3 - 0.00375 * t2 + 0.00209 * t1 + 0.00000) * turbidity * turbidity +
+                    (-0.02903 * t3 + 0.06377 * t2 - 0.03202 * t1 + 0.00394) * turbidity +
+                    (+0.11693 * t3 - 0.21196 * t2 + 0.06052 * t1 + 0.25886);
+                double yZen =
+                    (+0.00275 * t3 - 0.00610 * t2 + 0.00317 * t1 + 0.00000) * turbidity * turbidity +
+                    (-0.04214 * t3 + 0.08970 * t2 - 0.04153 * t1 + 0.00516) * turbidity +
+                    (+0.15346 * t3 - 0.26756 * t2 + 0.06670 * t1 + 0.26688);
+
+                Y = new SkylightDistCoef()
+                {
+                    A = +0.1787 * turbidity - 1.4630,
+                    B = -0.3554 * turbidity + 0.4275,
+                    C = -0.0227 * turbidity + 5.3251,
+                    D = +0.1206 * turbidity - 2.5771,
+                    E = -0.0670 * turbidity + 0.3703,
+                    Zenith = YZen,
+                };
+
+                _x = new SkylightDistCoef()
+                {
+                    A = -0.0193 * turbidity - 0.2592,
+                    B = -0.0665 * turbidity + 0.0008,
+                    C = -0.0004 * turbidity + 0.2125,
+                    D = -0.0641 * turbidity - 0.8989,
+                    E = -0.0033 * turbidity + 0.0452,
+                    Zenith = xZen,
+                };
+
+                _y = new SkylightDistCoef()
+                {
+                    A = -0.0167 * turbidity - 0.2608,
+                    B = -0.0950 * turbidity + 0.0092,
+                    C = -0.0079 * turbidity + 0.2102,
+                    D = -0.0441 * turbidity - 1.6537,
+                    E = -0.0109 * turbidity + 0.0529,
+                    Zenith = yZen,
+                };
+            }
+        }
+
+        private class SkylightDistCoef
+        {
+            public double A { get; set; }
+            public double B { get; set; }
+            public double C { get; set; }
+            public double D { get; set; }
+            public double E { get; set; }
+            public double Zenith { get; set; }
+        }
+    }
+}
